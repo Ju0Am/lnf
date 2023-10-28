ced9ac4c06f7a639374dae7f3bb1458243eccc7c
Ju0Am <149237351+Ju0Am@users.noreply.github.com>
2023-10-28 11:59:44 +0200

    init

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@200;400;600&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="home">
+        <img src="img/home.png" alt="bg" class="bg_home">
+        <div class="header">
+            <nav>
+                <label for="toggle">☰</label>
+                <input type="checkbox" id="toggle">
+                <div class="menu">
+                    <a href=""><img src="img/lnf.png" alt="lnf"></a>
+                    <a href="">Formation</a>
+                    <a href="">Accompagnement</a>
+                    <a href="">Formateur</a>
+                    <a href="">Société</a>
+                    <a href="">Achat</a>
+                    <a href="">Contact</a>
+                </div>
+            </nav>
+        </div>
+        <div class="txt_home">
+            <h1>LNF rend l’entrepreneuriat accessible à tous les cadres</h1>
+            <p>Des heures de formation à distance, <br>+ un programme d’accompagnement sur-mesure.</p>
+            <p class="little_txt">Prenez rendez-vous avec un conseiller LNF</p>
+            <button class="btn_home">Appelez-moi</button>
+        </div>
+    </div>
+
+    <div class="action">
+
+        <h1>PASSEZ ENSUITE À L’ACTION​</h1>
+        <p>Nous vous accompagnons gratuitement durant les 9 mois qui suivent la formation.​</p>
+
+        <div class="cardContainer">
+            <div class="card">
+                <span>1</span>
+                Préparez et testez votre projet
+            </div>
+            <div class="card">
+                Lancez-le et entreprenez
+                <span>2</span>
+            </div>
+            <div class="card">
+                Bénéficiez de notre expérience et de l’écosystème LNF​​
+                <span>3</span>
+            </div>
+        </div>
+
+
+        <ul class="list_action">
+            <li>Utilisez nos outils et suivez les chemins déjà balisés pour vous​</li>
+            <li>Évitez les erreurs et concentrez-vous dès le départ sur les points essentiels​.</li>
+            <li>Bénéficiez des avis et des commentaires d’entrepreneurs expérimentés​</li>
+            <li>Sollicitez nos partenaires et la communauté, et obtenez facilement des réponses à vos questions​</li>
+        </ul>
+    </div>
+
+    <div class="question_reponse">
+        <h1>TROIS QUESTIONS COURANTES</h1>
+        <table>
+            <tbody>
+                <tr>
+                    <td class="question">
+                        <span>QUESTION</span>
+                        Ça fait des années que j’ai envie d’entreprendre mais je n’avance pas sur ce
+                        sujet. ​À ce rythme je vais rester salarié toute ma vie. ​Est-ce que votre programme apporte une
+                        solution à ça ? »Préparez et testez votre projet
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="question">
+                        <span>RÉPONSE</span>
+                        ABSOLUMENT, c’est même pour résoudre ce sujet que ce programme existe.
+                    </td>
+
+                </tr>
+                <tr>
+                    <td class="question">
+                        <span>QUESTION</span>
+                        J’aimerais entreprendre mais j’ai besoin de mon salaire chaque mois. ​Ça me
+                    bloque pour me lancer. ​Votre programme apporte-t-il des solutions à ce sujet ?
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="question">
+                        <span>RÉPONSE</span>
+                        OUI, ce point est précisément traité dans le programme.
+                    </td>
+
+                </tr>
+                <tr>
+                    <td class="question">
+                        <span>QUESTION</span>
+                        « Je ne veux pas entreprendre en partant de zéro. Je veux racheter une
+                    entreprise qui fonctionne déjà et manager des équipes. ​Or je n’en trouve pas, et en plus je n’ai
+                    pas un
+                    gros
+                    apport personnel. Le programme LNF permet-il de résoudre ces points ? »
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="question">
+                        <span>RÉPONSE</span>
+                        OUI, ces deux sujets sont entièrement résolus dans le programme.
+                    </td>
+
+                </tr>
+            
+
+            </tbody>
+        </table>
+        <p>Posez-nous vos toutes vos questions : </p>
+        <button>Prenez rdv</button>
+    </div>
+
+    <div class="rejoindre">
+
+        <h1>QUAND PEUT-ON REJOINDRE LE PROGRAMME ENTREPRENEUR DE LNF ?</h1>
+
+        <p>DÉMARREZ QUAND VOUS VOULEZ - UNE NOUVELLE SESSION DÉMARRE CHAQUE SEMAINE ​</p>
+
+        <img src="img/fleche.png" alt="fleche">
+
+        <p class="txt_orange">Le programme LNF est sans contrainte : <br>​vous l’adaptez à votre rythme.​</p>
+
+    </div>
+
+</body>
+
+</html>
